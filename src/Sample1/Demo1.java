87ada05f0b92542aca1a32917fc45ee81aa448dc
Ojas <Ojas@DESKTOP-ETCNBJR>
2022-07-22 11:38:19 +0530

    [Rohan]: Added new project

---
+++ src/Sample1/Demo1.java
@@ -0,0 +1,15 @@
+package Sample1;
+
+public class Demo1 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hii");
+		
+		System.out.println("Hii");
+		
+		System.out.println("hello");
+		
+		System.out.println("hello");
+	}
+}
